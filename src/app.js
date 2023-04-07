2a5b5f0fc244d1f41f57472b5563edaddede6cda
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2023-04-06 19:16:22 -0600

    V.1.0.0 API REST

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use(indexRoutes)
-app.use("/api",userRoutes)
+app.use(userRoutes)
 
 app.use((req, res, next) => {
     res.status(404).json({ message: "Route not found" })
